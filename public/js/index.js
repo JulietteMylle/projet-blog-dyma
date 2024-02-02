d72e1f0a8e6f8fc6863a0ea379787f804dbcf286
Juliette Mylle <juliette.mylle@gmail.com>
2024-02-02 16:34:02 +0100

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+console.log("Hello Dyma !");
+
+const headerMobileButton = document.querySelector(".header-mobile-icon");
+const headerMobileList = document.querySelector(".header-mobile-list");
+headerMobileButton.addEventListener("click", () => {
+  headerMobileList.classList.toggle("show");
+});
